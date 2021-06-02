d34e0f7a28c66e8849d199dac128a3568e39f5f0
SmithRBG <smit36000@gmail.com>
2021-06-01 22:26:37 -0400

    Update

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-$(document).ready(function() {
-
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+$(document).ready(function() { //needed to declare JQuery, everything has to be inside
+ 
+    $("#currentDay").text(moment().format("dddd, MMMM Do")); //line for dates at the top of page
 
     $('.saveBtn').on('click', function() {
+
+
         var value = $(this).siblings('.description').val(); 
         var time = $(this).parent().attr('id');
-        localStorage.setItem(time, value);
 
-        $(".notification").addClass("show");
 
-        setTimeout(function () {
-            $(".notification").removeClass("show");   
+        localStorage.setItem(time, value); // puting above var in local storage
+
+        $(".notification").addClass("show"); //
+
+            setTimeout(function () {
+           $(".notification").removeClass("show");   
         }, 5000);
     });
 
@@ -20,12 +24,15 @@ $(document).ready(function() {
         $(".time-block").each(function() {
             var blockHour = parseInt($(this).attr("id").split("-")[1]);
             
-            if (blockHour < currentHour) { //Changes the classes to show where we are with time on the window
+            if (blockHour < currentHour) { 
+                //Changes the classes to show where we are with time on the window
+                //if blockHour is less than currentHour then add class past to 'this'
                 $(this).addClass("past");
             } else if (blockHour === currentHour) {
+                //else, if blackHour completely = currentHour remove the past CSS and add present CSS
                 $(this).removeClass("past");
                 $(this).addClass("present");
-            } else{
+            } else{ // If above doesn't = then let it be in the future.
                 $(this).removeClass("past");
                 $(this).removeClass("present");
                 $(this).addClass("future");
@@ -36,6 +43,9 @@ $(document).ready(function() {
 
     var interval = setInterval(hourUpdate, 15000);
 
+//local storage information will be loaded with below lines
+
+    
     $('#hour-8 .description').val(localStorage.getItem('hour-8'));
     $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
